170e511e9d2808362117a54548da13599031c331
Reed <reedesler+github@gmail.com>
2019-01-28 18:22:22 -0800

    Fix order of renderer

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,16 @@ int main(int argc, char *argv[]){
         }
         else
         {
-            auto renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-            auto drawContext = DrawContext(renderer);
             //Get window surface
             screenSurface = SDL_GetWindowSurface( window );
+
+            auto renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+            auto drawContext = DrawContext(renderer);
 
             //Fill the surface white
             SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 
-            //Update the surface
+          //Update the surface
             SDL_UpdateWindowSurface( window );
 
 
