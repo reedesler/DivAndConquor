e7fd646f0fb06141a507097a6c62e93348c917d8
Tomer Torgeman <tomertorgeman@dhcp-128-189-66-249.ubcsecure.wireless.ubc.ca>
2019-01-30 16:57:33 -0800

    classes

---
+++ src/Pirate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomer Torgeman on 2019-01-30.
+//
+
+#include "Pirate.h"
---
+++ src/Pirate.h
@@ -0,0 +1,18 @@
+//
+// Created by Tomer Torgeman on 2019-01-30.
+//
+
+#ifndef DIVCONQ_PIRATE_H
+#define DIVCONQ_PIRATE_H
+
+
+class Pirate {
+
+
+
+
+
+};
+
+
+#endif //DIVCONQ_PIRATE_H
