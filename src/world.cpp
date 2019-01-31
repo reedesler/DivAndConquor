1b00ccb14e0faa3e2a5568df29b95d281b0468b4
David Ma <qingnian213@hotmail.com>
2019-01-30 16:22:02 -0800

    Added save & load GameArchive

--- src/world.cpp
+++ src/world.cpp
@@ -1,24 +1,61 @@
 //
 // Created by Dmitry on 2019-01-25.
 //
+#include <fstream>
 
 #include "world.h"
 
-World::World(int w, int h, Camera* camera) : camera(camera) {
+World::World(int w, int h, Camera *camera) : camera(camera)
+{
     this->tilemap.resize(w, std::vector<int>(h, 0));
 }
 
 World::~World() = default;
 
-void World::update() {
+void World::update()
+{
     camera->update();
 }
 
-void World::draw(DrawContext* dc) {
+void World::draw(DrawContext *dc)
+{
     camera->draw(dc);
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++)
+    {
         camera->drawRect({200 + 10 * i, 200, 10, 10}, {0, 255, 255, 255});
     }
     camera->drawRect({50, 50, 100, 100}, {0, 255, 255, 255});
     camera->drawRect({75, 0, 50, 50}, {0, 255, 0, 0});
+}
+
+void World::save(std::string archiveName)
+{
+    // create and open a character archive for output
+    std::ofstream ofs(archiveName + ".dnq");
+
+    // create class instance
+    const GameArchive g();
+
+    // save data to archive
+    {
+        boost::archive::text_oarchive oa(ofs);
+        // write class instance to archive
+        oa << g;
+        // archive and stream closed when destructors are called
+    }
+}
+
+GameArchive World::load(std::string fileName)
+{
+    // ... some time later restore the class instance to its orginal state
+    GameArchive newg;
+    {
+        // create and open an archive for input
+        std::ifstream ifs(fileName);
+        boost::archive::text_iarchive ia(ifs);
+        // read class state from archive
+        ia >> newg;
+        // archive and stream closed when destructors are called
+    }
+    return newg;
 }
--- src/world.h
+++ src/world.h
@@ -6,24 +6,67 @@
 #define DIVCONQ_WORLD_H
 
 #include <vector>
+#include <string>
+
+// include headers that implement a archive in simple text format
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
 #include "drawable.h"
 #include "Camera.h"
 
-class World : Drawable {
+class World : Drawable
+{
 public:
-    World(int w, int h, Camera *camera);
+  World(int w, int h, Camera *camera);
 
-    ~World();
+  ~World();
 
-    void update() override;
+  void update() override;
 
-    void draw(DrawContext *dc) override;
+  void draw(DrawContext *dc) override;
 
-    Camera *camera;
+  Camera *camera;
+
+  void save(std::string archiveName);
+
+  GameArchive load(std::string fileName);
 
 private:
-    std::vector<std::vector<int>> tilemap;
+  std::vector<std::vector<int>> tilemap;
+  std::vector<std::string> savedFiles;
 };
 
+struct Action
+{
+};
+
+struct GameObject
+{
+};
+
+// illustrates serialization for the game object
+class GameArchive
+{
+private:
+  friend class boost::serialization::access;
+  // When the class Archive corresponds to an output archive, the
+  // & operator is defined similar to <<.  Likewise, when the class Archive
+  // is a type of input archive the & operator is defined similar to >>.
+  template <class Archive>
+  void serialize(Archive &ar, const unsigned int version)
+  {
+    ar &objects;
+    ar &actions;
+  }
+  std::vector<Action> currentActions;
+  std::vector<GameObject> gameObjects;
+
+public:
+  GameArchive(){};
+  GameArchive(std::vector<Action> actions, std::vector<GameObject> objects) : currentActions(actions), gameObjects(objects)
+  {
+  }
+};
 
 #endif //DIVCONQ_WORLD_H
