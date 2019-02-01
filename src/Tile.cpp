14b6ddefbb6cd0d1ddd89f914a5236baed034733
Vova <vehsuga@gmail.com>
2019-02-01 04:45:17 -0800

    tile class

---
+++ src/Tile.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Vova on 2019-01-31.
+//
+
+#include "Tile.h"
+
+Tile::Tile(int x, int y, int type, SDL_Texture *texture){
+    mbox.x = x;
+    mbox.y = y;
+    tType = type;
+    tTexture = texture;
+    mbox.h = TILE_SIDE;
+    mbox.w = TILE_SIDE;
+    tVisible = 1;
+}
+
+Tile::~Tile() = default;
+
+//getters
+int Tile::getType() {
+    return this->tType;
+}
+
+int Tile::isVisible() {
+    return this->tVisible;
+}
+
+SDL_Texture *Tile::getTex() {
+    return this->tTexture;
+}
+
+SDL_Rect *Tile::getBox() {
+    return &this->mbox;
+}
+
+//loading assets for the map.
+//here until we implement some asset management
+
+void buildMap(std::vector<std::vector<Tile *>> *map, std::vector<SDL_Texture *> *maptex, DrawContext *dc) {
+
+    //creating textures
+    //should be improved
+    SDL_Surface *loadedSurface = SDL_LoadBMP("../assets/mapgen/water00.bmp");
+    maptex->push_back(SDL_CreateTextureFromSurface( dc->Renderer, loadedSurface ));
+    free(loadedSurface);
+    loadedSurface = SDL_LoadBMP("../assets/mapgen/sand00.bmp");
+    maptex->push_back(SDL_CreateTextureFromSurface( dc->Renderer, loadedSurface ));
+    free(loadedSurface);
+    loadedSurface = SDL_LoadBMP("../assets/mapgen/grass00.bmp");
+    maptex->push_back(SDL_CreateTextureFromSurface( dc->Renderer, loadedSurface ));
+    free(loadedSurface);
+
+    //loading map txt
+    std::ifstream mapFile("../assets/mapgen/map1test.txt");
+
+    //initializing tiles
+    int type;
+    for (int i = 0; i < TOTAL_SIDE; i++){
+        std::vector<Tile *> tmp;
+        for (int j = 0; j < TOTAL_SIDE; j++){
+            type = mapFile.get() - 48;
+            if (type == 3)
+                type = 2;
+            tmp.push_back(new Tile(j * TILE_SIDE, i * TILE_SIDE, type, (*maptex)[type]));
+        }
+        mapFile.get();
+        map->push_back(tmp);
+    }
+}
---
+++ src/Tile.h
@@ -0,0 +1,47 @@
+//
+// Created by Vova on 2019-01-31.
+//
+
+#ifndef DIVCONQ_TILE_H
+#define DIVCONQ_TILE_H
+
+#include <SDL.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "draw_context.h"
+
+//Tile constants
+const int TILE_SIDE = 100;
+const int TOTAL_SIDE = 500;
+const int TOTAL_TILE_TYPES = 3;
+
+class Tile
+{
+public:
+    Tile(int x, int y, int type, SDL_Texture *texture);
+
+
+    int getType();
+    SDL_Rect *getBox();
+    SDL_Texture *getTex();
+    int isVisible();
+
+
+private:
+
+    int tType;
+
+    int tVisible;
+
+    SDL_Rect mbox;
+
+    SDL_Texture *tTexture;
+
+    ~Tile();
+
+};
+
+void buildMap(std::vector<std::vector<Tile *>> *map, std::vector<SDL_Texture *> *maptex, DrawContext *dc);
+#endif //DIVCONQ_TILE_H
