4e040c905480600ce7d11d90d07ddd89b4d45816
David Ma <qingnian213@hotmail.com>
2019-02-14 16:18:31 -0800

    Changed filenames

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,9 +1,12 @@
-#include "Sprite.h"
+#include "Sprite.hpp"
 
-bool Sprite::init(float width, float height) {
+bool Sprite::init(float width, float height)
+{
     // Load shared texture
-    if (!texture.is_valid()) {
-        if (!texture.load_from_file(textures_path("turtle.png"))) {
+    if (!texture.is_valid())
+    {
+        if (!texture.load_from_file(textures_path("turtle.png")))
+        {
             fprintf(stderr, "Failed to load texture!");
             return false;
         }
@@ -48,8 +51,8 @@ bool Sprite::init(float width, float height) {
     return effect.load_from_file(shader_path("textured.vs.glsl"), shader_path("textured.fs.glsl"));
 }
 
-
-void Sprite::draw(const mat3 &projection, vec2 position, float rotation, vec2 scale) {
+void Sprite::draw(const mat3 &projection, vec2 position, float rotation, vec2 scale)
+{
     // Transformation code, see Rendering and Transformation in the template specification for more info
     // Incrementally updates transformation matrix, thus ORDER IS IMPORTANT
     transform_begin();
@@ -81,18 +84,18 @@ void Sprite::draw(const mat3 &projection, vec2 position, float rotation, vec2 sc
     GLint in_texcoord_loc = glGetAttribLocation(effect.program, "in_texcoord");
     glEnableVertexAttribArray(in_position_loc);
     glEnableVertexAttribArray(in_texcoord_loc);
-    glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *) 0);
-    glVertexAttribPointer(in_texcoord_loc, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *) sizeof(vec3));
+    glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *)0);
+    glVertexAttribPointer(in_texcoord_loc, 2, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void *)sizeof(vec3));
 
     // Enabling and binding texture to slot 0
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture.id);
 
     // Setting uniform values to the currently bound program
-    glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float *) &transform);
+    glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float *)&transform);
     float color[] = {1.f, 1.f, 1.f};
     glUniform3fv(color_uloc, 1, color);
-    glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float *) &projection);
+    glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float *)&projection);
 
     // Drawing!
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
