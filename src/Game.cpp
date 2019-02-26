2721d4846bcde8f201d36302b40d27b495a55a7f
David Ma <qingnian213@hotmail.com>
2019-02-25 17:39:02 -0800

    Changed Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -51,14 +51,6 @@ void Game::init(vec2 screen)
 
     registerButton(build_ship_button, {80.f, 100.f}, invokeBuildShip);
     registerButton(hire_sailors_button, {80.f, 500.f}, invokeHireSailors);
-
-    FT_Library ft;
-    if (FT_Init_FreeType(&ft))
-        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-
-    FT_Face face;
-    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
-        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
 }
 
 void Game::update()
